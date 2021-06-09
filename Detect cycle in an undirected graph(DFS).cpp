f1b2de8d224608104688331aee5a7965a7487d03
anjalisankar4 <85636353+anjalisankar4@users.noreply.github.com>
2021-06-10 01:13:05 +0530

    Add files via upload

---
+++ Detect cycle in an undirected graph(DFS).cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isCyclic(int node, int parent, vector<vector < int>> &adjList, vector< bool > &visited)
+{
+	visited[node] = true;
+	int neighbours = adjList[node].size();
+	for (int i = 0; i < neighbours; i++)
+	{
+		int neighbourNode = adjList[node][i];
+		if (!visited[neighbourNode])
+		{
+			if (isCyclic(neighbourNode, node, adjList, visited) == true)
+			{
+				return true;
+			}
+		}
+		else if (neighbourNode != parent)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void addEdge(vector<vector < int>> &adjList, int x, int y)
+{
+	adjList[x].push_back(y);
+	adjList[y].push_back(x);
+}
+
+int main()
+{
+	int n, m;
+	cout << "Enter number of nodes and edges: " << endl;
+	cin >> n >> m;
+	vector<bool> visited(n, false);
+	vector<bool> inStack(n, false);
+	vector<vector < int>> adjList(n);
+	cout << "Enter edges: " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+		addEdge(adjList, u, v);
+
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (!visited[i])
+		{
+			if (isCyclic(i, -1, adjList, visited))
+			{
+				cout << "Graph has a cycle" << endl;
+				return 0;
+			}
+		}
+	}
+
+	cout << "Graph doesn't have a cycle" << endl;
+}
